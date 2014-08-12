4951dfc480da52b0955445b9055293ea8e3e4534
Thomas Pedersen <tp@adaheads.com>
2014-08-12 07:55:28 +0200

    Changed from a "fire and forget" commands, to waiting for the futures.

--- examples/testSocket.dart
+++ examples/testSocket.dart
@@ -1,61 +1,61 @@
+import 'dart:async';
+
 import '../lib/esl.dart' as ESL;
 
 ESL.PeerList peerList = null;
 
 main() {
-  
+
   ESL.Connection conn = new ESL.Connection();
-  ESL.ChannelList channelList = new ESL.ChannelList(); 
-  
+  ESL.ChannelList channelList = new ESL.ChannelList();
+
   conn.eventStream.listen((ESL.Packet packet) {
-      switch (packet.eventName) {
-        case ("CHANNEL_STATE"):
-          channelList.update(new ESL.Channel.fromPacket(packet));
-          break;
-        case ("CHANNEL_CREATE"):
-          break;
-      }
-  });
-  
-  conn.requestStream.listen((ESL.Packet packet) {
-    switch (packet.contentType) {
-      case (ESL.ContentType.Auth_Request):
-      
-      conn.authenticate('1234').then((packet) => print(packet.headers))
-        .then((_) => conn.event(['all'], format : ESL.EventFormat.Json))
-        .then((_) => conn.api('status').then(print))
-        .then((_) => conn.api('list_users')
-        .then((packet) => print(new ESL.PeerList.fromMultilineBuffer(packet.rawBody)))
-        //.then((_) => conn.api ('originate user/1100 5900').then(print).catchError(print))
-        .then((_) {
-          for (int i = 0; i < 100; i++) {
-            sendRequest(i, conn);
-          }
-        }));
-      
-      break;
-      default:
-        
+    switch (packet.eventName) {
+      case ("CHANNEL_STATE"):
+        channelList.update(new ESL.Channel.fromPacket(packet));
+        break;
+      case ("CHANNEL_CREATE"):
         break;
     }
   });
-  
+
+  conn.requestStream.listen((ESL.Packet packet) {
+    switch (packet.contentType) {
+      case (ESL.ContentType.Auth_Request):
+
+      conn.authenticate('1234').then((packet) => print(packet.headers))
+        .then((_) => conn.event(['all'], format : ESL.EventFormat.Json))
+        .then((_) => conn.api('status').then(print))
+        .then((_) => conn.api('list_users'))
+        .then((packet) => print(new ESL.PeerList.fromMultilineBuffer(packet.rawBody)))
+        //.then((_) => conn.api ('originate user/1100 5900').then(print).catchError(print))
+        .then((_) {
+          List<int> sequence = new List.generate(100, (int index) => index);
+          return Future.wait(sequence.map((int i) => sendRequest(i, conn)));
+        });//.then((_) => conn.disconnect());
+
+        break;
+      default:
+
+        break;
+    }
+  });
+
   void signalDisconnect() => print('Disconnected!');
 
   conn..onDone = signalDisconnect
       ..connect('localhost', 8021);
-
 }
 
 
-void sendRequest (int seq, ESL.Connection conn) {
-  conn.api('echo $seq').then((ESL.Response response) {
+Future sendRequest (int seq, ESL.Connection conn) {
+  return conn.api('echo $seq').then((ESL.Response response) {
     print('$seq, ${response.rawBody}');
     assert (int.parse(response.rawBody) == seq);
-    });
+  });
 }
-  
+
 
 void loadPeerListFromPacket (ESL.Packet packet) {
- peerList = new ESL.PeerList.fromMultilineBuffer(packet.content);
+  peerList = new ESL.PeerList.fromMultilineBuffer(packet.content);
 }
