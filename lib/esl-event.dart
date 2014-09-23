dd395c8704133270fe88046dcca9271b186ff97a
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-09-23 09:22:31 +0200

    Headers of packets were not properly forwarded to events upon construction.

--- lib/esl-event.dart
+++ lib/esl-event.dart
@@ -3,9 +3,10 @@ part of esl;
 class Event extends Packet {
   String  _name    = null;
   Channel _channel = null;
-  
+
   Event.fromPacket (Packet packet) {
-    this.content = packet.content; 
+    this.headers = packet.headers;
+    this.content = packet.content;
   }
-  
+
 }
