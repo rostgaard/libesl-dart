5d7b6de3f7e7668beffe5d036d49b12aa99c561b
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-02-23 14:50:01 +0100

    Removal of unused fields and adding documentation.

--- lib/esl-response.dart
+++ lib/esl-response.dart
@@ -1,9 +1,9 @@
 part of esl;
 
+/**
+ * Class representing a reponse received from the FreeSWTICH event socket.
+ */
 class Response {
-
-  static int     _count   = 0;
-  static int get sequence => ++_count;
 
   static const String OK      = '+OK';
   static const String ERROR   = '-ERR';
@@ -13,6 +13,9 @@ class Response {
 
   Response.fromPacketBody(String this.rawBody);
 
+  /**
+   * The status of the response. Can be either [OK], [ERROR] or [UNKNOWN].
+   */
   String get status {
     String lastLine = this.rawBody.split('\n').last;
 
@@ -25,16 +28,24 @@ class Response {
     }
   }
 
+  /**
+   * Reponses may carry the UUID of a channel.
+   */
   String get channelUUID {
     String lastLine = this.rawBody.split('\n').last;
 
     if (lastLine.startsWith(OK)) {
       return lastLine.substring(OK.length, lastLine.length).trim();
     } else {
-      throw new StateError('Response does not carry channel information. Raw body: ${this.rawBody}');
+      throw new StateError('Response does not carry channel information. '
+                           'Raw body: ${this.rawBody}');
     }
   }
 
+  /**
+   * String representation of a Response for debug purposes or
+   * manual processing.
+   */
   @override
   String toString() {
     return this.rawBody;
