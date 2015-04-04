0b7e3c1bdbb7cbe0336a8e1c266adb91ff96d082
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-04 20:49:16 +0200

    Documentation.

--- lib/src/channel_list.dart
+++ lib/src/channel_list.dart
@@ -1,11 +1,21 @@
 part of esl;
 
+/**
+ * Channel list. Can be used to keep track of channels in the FreeSWITCH PBX by
+ * using the [update] method as event handler for CHANNEL_STATE and others,
+ * depending on the level of detail in the channel information you wish.
+ */
 class ChannelList extends IterableBase<Channel> {
 
   Map<String, Channel> _channelStorage = new Map<String, Channel>();
 
   Iterator<Channel> get iterator => this._channelStorage.values.iterator;
 
+  /**
+   * Updates a channel stored in the channel list. Removes it, if it is
+   * destroyed, adds it when it is created and merely updates the information
+   * associated with channel (variables and fields) with the new information.
+   */
   void update(Channel channel) {
     if (!this._channelStorage.containsKey(channel.UUID)) {
       this._add(channel);
@@ -30,6 +40,10 @@ class ChannelList extends IterableBase<Channel> {
     this._channelStorage[channel.UUID] = channel;
   }
 
+  /**
+   * Retrieve a single channel identified by [channelID]. Returns [null] if no
+   * channel is found in the list.
+   */
   Channel get(String channelID) => this._channelStorage[channelID];
 
 
