cb0fc30cc85a099cec1bfde5b1ebcd3ad3413a3d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-09-08 18:54:47 +0200

    Copyright headers.

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, Kim Rostgaard Christensen. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 part of esl;
 
 class Request extends Packet {
