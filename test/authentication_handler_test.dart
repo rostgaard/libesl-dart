55ee8472c5847e3624d3c606368118f7b9669733
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-07-16 21:41:56 +0200

    Tests of the authentication handler.

---
+++ test/authentication_handler_test.dart
@@ -0,0 +1,60 @@
+// Copyright (c) 2015, Kim Rostgaard Christensen. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+library esl.test;
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:logging/logging.dart';
+import 'package:test/test.dart';
+import 'package:esl/esl.dart' as esl;
+import 'src/dummy_esl.dart';
+
+part 'peer_list.dart';
+part 'response.dart';
+
+main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(print);
+  group('AuthenticationHandler', () {
+    test('correct password', _correctPassword);
+    test('wrong password', _wrongPassword);
+  });
+}
+
+Future _correctPassword() async {
+  final String password = 'dummy';
+  final esl.Connection connection = new esl.Connection();
+  final DummyEsl dummyEsl =
+      new DummyEsl(await ServerSocket.bind('127.0.0.1', 18021), password);
+
+  await connection.connect('127.0.0.1', 18021);
+
+  Future authentication = esl.authHandler(connection, password);
+
+  await authentication;
+  await connection.disconnect();
+  await dummyEsl.close();
+}
+
+Future _wrongPassword() async {
+  final String password = 'dummy';
+  final esl.Connection connection = new esl.Connection();
+  final DummyEsl dummyEsl =
+      new DummyEsl(await ServerSocket.bind('127.0.0.1', 18021), password);
+
+  await connection.connect('127.0.0.1', 18021);
+
+  Future authentication = esl.authHandler(connection, password + 'wrong');
+
+  try {
+    await authentication;
+  } on esl.AuthenticationFailure {
+    // Expected result.
+  }
+
+  await connection.disconnect();
+  await dummyEsl.close();
+}
---
+++ test/src/dummy_esl.dart
@@ -0,0 +1,99 @@
+library esl.test_support.dummy_esl;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:logging/logging.dart';
+
+class _ClientConnection {
+  final Socket socket;
+  bool isAuthenticated = false;
+
+  _ClientConnection(this.socket);
+}
+
+/// ESL dummy server that emulates the behaviour of a real FreeSWITCH ESL.
+class DummyEsl {
+  final Logger _log = new Logger('DummyEsl');
+  final ServerSocket _socket;
+  final String _password;
+  final Set<_ClientConnection> _clients = new Set();
+
+  DummyEsl(this._socket, this._password) {
+    _log.info('Server listening on ${_socket.address.address}:${_socket.port}');
+
+    _socket.listen((Socket clientSocket) {
+      _log.info('New client from ${clientSocket.address}');
+      _ClientConnection client = new _ClientConnection(clientSocket);
+
+      _clients.add(client);
+
+      _authenticationChallenge(client);
+    },
+        onDone: () => _log.info(
+            'Shutting down socket ${_socket.address.address}:${_socket.port}'));
+  }
+
+  void _send(_ClientConnection client, List<String> lines) {
+    try {
+      for (String line in lines) {
+        client.socket.writeln(line);
+      }
+      client.socket.writeln();
+    } catch (e) {
+      _log.warning('Failed to send lines to client $client', e);
+    }
+  }
+
+  ///
+  _authenticationChallenge(_ClientConnection client) async {
+    _log.finest('Sending authentication challenge');
+    _send(client, ['Content-Type: auth/request']);
+
+    client.socket
+        .transform(ASCII.decoder)
+        .transform(const LineSplitter())
+        .where((String buffer) => buffer.isNotEmpty)
+        .listen(
+            (String buffer) {
+              final List<String> parts = buffer.split(' ');
+              final command = parts.first;
+
+              switch (command) {
+                case 'auth':
+                  final String password =
+                      parts.length > 1 ? parts[1].trim() : '';
+
+                  if (password != _password) {
+                    _log.finest('password not ok');
+                    _send(client,
+                        ['Content-Type: command/reply', 'Reply-Text: +ERROR']);
+                  } else {
+                    _log.finest('password ok');
+                    _send(client, [
+                      'Content-Type: command/reply',
+                      'Reply-Text: +OK accepted'
+                    ]);
+                    client.isAuthenticated = true;
+                  }
+              }
+            },
+            onError: (e) => _log.warning('Client listener error', e),
+            onDone: () {
+              _log.info('Client ${client.socket.address.address} disconnected');
+            });
+  }
+
+  ///
+  Future close() async {
+    for (_ClientConnection client in _clients) {
+      try {
+        client.socket.close();
+      } catch (e) {
+        _log.warning('Failed to close socket', e);
+      }
+    }
+    await _socket.close();
+  }
+}
