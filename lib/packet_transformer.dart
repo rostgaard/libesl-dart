7af089c0661eeaa6c4fb4231ea1ebd990de71c8b
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-07-16 21:27:59 +0200

    Updated documentation to match style guide better. #11

--- lib/packet_transformer.dart
+++ lib/packet_transformer.dart
@@ -7,15 +7,14 @@ import 'dart:async';
 
 import 'package:esl/esl.dart';
 
-/**
- * Transformer for converting raw bytes into [Packet] objects.
- * Handles the low-level parsing of network traffic, or any other byte stream
- * for that matter, stores them in a buffer and encapsulates them in [Packet]
- * objects which the [Connection] then re-casts and injects into the
- * appropriate steam - for instance event stream.
- *
- * Can also be used standalone for parsing, for instance, packet dumps.
- */
+/// Transformer for converting raw bytes into [Packet] objects.
+///
+/// Handles the low-level parsing of network traffic, or any other byte
+/// stream for that matter, stores them in a buffer and encapsulates them
+/// in [Packet] objects for which a [Connection] object may re-casts them
+/// and forward them to the appropriate steam - for instance event stream.
+///
+/// Can also be used standalone for parsing, for instance, packet dumps.
 class PacketTransformer implements StreamTransformer<List<int>, Packet> {
   final StreamController<Packet> _controller = new StreamController<Packet>();
   Packet _currentPacket = new Packet();
@@ -26,16 +25,16 @@ class PacketTransformer implements StreamTransformer<List<int>, Packet> {
   int _currentChar;
   static final int _newLine = '\n'.codeUnits.first;
 
+  ///Transform the incoming `stream's` events into [Packet] object.
   @override
   Stream<Packet> bind(Stream<List<int>> stream) {
     stream.listen(_onData, onDone: _controller.close);
     return _controller.stream;
   }
 
-  /**
-   * Callback for receiving and processing bytes.
-   * Supports segmented transfers, such as TCP buffers.
-   */
+  /// Callback for receiving and processing bytes.
+  ///
+  /// Supports segmented transfers, such as TCP buffers.
   void _onData(List<int> bytes) {
     for (int offset = 0; offset < bytes.length; offset++) {
       int lastChar = _currentChar;
@@ -55,7 +54,7 @@ class PacketTransformer implements StreamTransformer<List<int>, Packet> {
                     'Bad header received: ${_currentPacket.headers}'));
               }
 
-              /// Skip empty lines.
+              // Skip empty lines.
               else if (_currentPacket.headers.isNotEmpty) {
                 _controller.sink.add(_currentPacket);
               }
@@ -64,7 +63,7 @@ class PacketTransformer implements StreamTransformer<List<int>, Packet> {
           } else {
             String headerLine = new String.fromCharCodes(_headerBuffer);
 
-            /// Ignore short lines.
+            // Ignore short lines.
             if (headerLine.length > 1) {
               int splitIndex = headerLine.indexOf(':');
               if (splitIndex > 0) {
@@ -89,10 +88,10 @@ class PacketTransformer implements StreamTransformer<List<int>, Packet> {
           _currentPacket.content = new String.fromCharCodes(_bodyBuffer);
           _readingHeader = true;
 
-          /// Sink on the packet.
+          // Sink on the packet.
           _controller.sink.add(_currentPacket);
 
-          /// Clear the state.
+          // Clear the state.
           _bodyBuffer = [];
           _currentPacket = new Packet();
         }
